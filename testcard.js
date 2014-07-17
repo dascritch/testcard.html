2ee6661ed7f420e718142278d1aee9a7341258b5
dascritch <xavier@dascritch.com>
2014-07-17 02:32:35 +0200

    primitives for scenes playlist

--- testcard.js
+++ testcard.js
@@ -6,3 +6,19 @@ function testcard_pixels_check() {
 
 testcard_pixels_check();
 window.addEventListener('resize',testcard_pixels_check)
+
+var data = document.querySelector('script[type="text/json"]');
+var main = document.querySelector('main');
+var parameters;
+if (data !== null) {
+	parameters = JSON.parse(data.innerHTML);
+}
+var index = 0;
+
+for (var index in parameters) {
+	var scene = parameters[index];
+	console.log('scene ' , index,scene)
+	if (scene.video !== undefined) {
+		main.innerHTML = '<video src="'+scene.video+'" autoplay></video>';
+	}
+}
