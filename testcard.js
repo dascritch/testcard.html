f8c7ba1b465b5b4e13e86482d2ab07747345ccbe
dascritch <xavier@dascritch.com>
2014-07-26 17:29:32 +0200

    Adding scene switching. can't wait anymore ^^

--- testcard.js
+++ testcard.js
@@ -1,51 +1,87 @@
+(function(){
+"use strict";
+
+var defaults = {
+		back		: '#777777',
+		charts		: ["contrast" , "sharpness" , "colour"]
+}
+
+var parameters;
+var main = document.querySelector('main');
+var data = document.querySelector('script[type="text/json"]');
+var scene_index = 0;
+
+function mergeArrays(obj1,obj2) {
+	var out = {};
+	for(var i in obj1) {
+		out[i] = obj1[i];
+	}
+	for(var i in obj2) {
+		out[i] = obj2[i];
+	}
+	return out;
+}
+
 function testcard_pixels_check() {
 	document.getElementById('pixels_horizontal').innerHTML = window.innerWidth;
 	document.getElementById('pixels_vertical').innerHTML = window.innerHeight;
 	document.getElementById('pixels_ratio').innerHTML = window.devicePixelRatio.toFixed(2);
 }
 
-testcard_pixels_check();
-window.addEventListener('resize',testcard_pixels_check)
+function testcard(scene) {
+	var scenesTypes = ['img','youtube','video'];
 
-var data = document.querySelector('script[type="text/json"]');
-var main = document.querySelector('main');
-var parameters;
-if (data !== null) {
-	parameters = JSON.parse(data.innerHTML);
-}
-var index = 0;
-var scenesTypes = ['img','youtube','video'];
+	// setting background
+	main.style.backgroundColor = scene.back;
+	main.innerHTML='';
 
-function mergewith(obj1,obj2) {
-	for(var i in obj2) {
-		obj1[i] = obj2[i];
-	}
-	return obj1;
-}
-
-var defaults = {
-	back : '#777777'
-}
-
-for (var index in parameters.scenes) {
-	console.log( parameters.default , parameters.scenes[index], parameters.default + parameters.scenes[index])
-	var scene = mergewith( defaults , mergewith( parameters.default , parameters.scenes[index]) );
-	if (scene.back !== undefined) {
-		main.style.backgroundColor = scene.back;
-	}
+	var has = false;
 	for (var t in scenesTypes) {
 		var mode = scenesTypes[t];
-		if (scene[mode] !== undefined) {
+		if ((!has) && (scene[mode] !== undefined)) {
 			switch (mode) {
-					/*
 				case 'img' :
 					main.innerHTML = '<img src="'+scene.img+'" alt="" class="fullCroped" />';
 					break;
 				case 'video' :
 					main.innerHTML = '<video src="'+scene.video+'" autoplay class="fullAdapt"></video>';
 					break;
-					*/
+				case 'youtube' :
+					main.innerHTML = '<iframe width="100%" height="100%" src="http://www.youtube-nocookie.com/embed/'+scene.youtube+'?rel=0" frameborder="0" allowfullscreen></iframe>';
+					break;
 			}
+			has = true;
 		}
 	}
 }
+
+function goto_scene(scene_index) {
+	var scene = mergeArrays( defaults , mergeArrays( parameters.default , parameters.scenes[scene_index]) );
+	testcard(scene);
+}
+
+function keyboard(event) {
+		switch ( event.keyCode ) {
+		case 37 :
+			scene_index = scene_index < 0 ? parameters.scenes.length : --scene_index;
+			goto_scene(scene_index);
+			break;
+		case 39 :
+			scene_index = scene_index > parameters.scenes.length ? 0 : ++scene_index;
+			goto_scene(scene_index);
+			break;
+	}
+}
+
+if (data !== null) {
+	parameters = JSON.parse(data.innerHTML);
+}
+
+goto_scene(scene_index);
+testcard_pixels_check();
+
+document.addEventListener('keydown',keyboard);
+window.addEventListener('resize',testcard_pixels_check)
+
+
+})();
